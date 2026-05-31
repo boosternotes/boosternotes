d358caa48d0fb97bb7724b1ee7bd9318072a31b4
boosternotes <alishav112233@gmail.com>
2026-05-31 20:55:58 +0530

    Initial commit

--- boosternotes/settings.py
+++ boosternotes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g)(h61zja22%r+hx-l=x2jia$agp8_a+)+_p7nx7sb!r)b(f15'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
