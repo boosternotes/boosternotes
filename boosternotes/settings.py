ba3be1945f1032248a38123c72699e22dfbd76c8
boosternotes <alishav112233@gmail.com>
2026-06-03 18:08:00 +0530

    Update settings.py

--- boosternotes/settings.py
+++ boosternotes/settings.py
@@ -53,16 +53,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'boosternotes.wsgi.application'
 
-import os
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('postgresql://postgres:lZGGoknFetoVMsoQMbRPMARpzrmRNXxd@zephyr.proxy.rlwy.net:30602/railway'),
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
